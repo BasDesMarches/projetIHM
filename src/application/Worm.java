be88348b8ffb9e95fe594db310a9da395f70b19e
Hugo <Hugo@PCHugo>
2015-05-21 11:10:05 +0200

    Addition of class Worm and Weapon

---
+++ src/application/Worm.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Worm {
+	private String name;
+	private SimpleIntegerProperty life;
+	private Weapon[] weapons;
+	private SimpleIntegerProperty xPos;
+	private SimpleIntegerProperty yPos;
+	private int team;
+	private int numInTeam;
+
+}
