52b4bcce49e3ede1de42b7b6c64d38064fa70c44
Hugo <Hugo@10.65.223.243>
2015-05-24 20:58:41 +0200

    elementalmove for worms added

--- src/application/Worm.java
+++ src/application/Worm.java
@@ -8,6 +8,36 @@ public class Worm {
 	private Weapon[] weapons;
 	private SimpleIntegerProperty xPos;
 	private SimpleIntegerProperty yPos;
+	private int paceCounter;
 	//new Team team;
+	
+	public Worm(String name, int x, int y){
+		this.name=name;
+		xPos=new SimpleIntegerProperty(x);
+		yPos=new SimpleIntegerProperty(y);
+		life=new SimpleIntegerProperty(100);
+	}
+	
+	private void elementalMove(boolean isRight, char[][]getMap){
+		
+		//move right or left
+		if (isRight){
+			xPos.add(1);
+		}else{
+			xPos.add(-1);
+		}
+		//adjust the hight
+		if (getMap[xPos.get()][yPos.get()]==('0')){
+			while(getMap[xPos.get()][yPos.get()-1]==('0'))
+			{
+				yPos.add(-1);
+			}
+		}else{
+			while(getMap[xPos.get()][yPos.get()-1]==('1')){
+				yPos.add(1);
+			}
+		}
+		paceCounter--;
+	}
 
 }
